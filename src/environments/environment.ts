aecb3787c24f2a49bb61b7ffd31e1fba44a2c82e
u21482358 <u21482358@tuks.co.za>
2025-08-07 15:51:25 +0200

    fixed environment prod file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     
-     apiUrl: 'orderingsystemfrontend-h3h7hzaxfbgzdjav.southafricanorth-01.azurewebsites.net/api',
+     apiUrl: 'orderingsystemfrontend-h3h7hzaxfbgzdjav.southafricanorth-01.azurewebsites.net/api/',
      
      production: true
 };
